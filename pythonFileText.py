30b79cc9e9c47acc22e5cf2250557aa84acd3f71
Mubashir Iqbal <mubshr07@gmail.com>
2018-07-23 19:16:20 +0500

    Saving progress on 2018-07-23 19:16:20 time 2171

--- pythonFileText.py
+++ pythonFileText.py
@@ -6022,3 +6022,11 @@
             return False
     return True
 
+ def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
