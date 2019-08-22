aada59c4cac514e9c0811d57851ea8c28d879a9c
Mubashir Iqbal <mubshr07@gmail.com>
2019-08-22 19:26:26 +0500

    Saving progress on 2019-08-22 19:26:26 time 1776

--- jsFileText.js
+++ jsFileText.js
@@ -11821,3 +11821,10 @@ const data = {
 const template = templater(tmpl);
 document.write(template(data));
 
+const tmpl = "<h1>Hi, I'm {{name}}.</h1>";
+const data = {
+  name: "Mubashir Iqbal"
+};
+const template = templater(tmpl);
+document.write(template(data));
+
