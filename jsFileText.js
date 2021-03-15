bdbe9e57dbaaad6a7091075ea8ebc38e80918477
Mubashir Iqbal <mubshr07@gmail.com>
2021-03-15 19:43:23 +0500

    Saving progress on 2021-03-15 19:43:23 time 1205

--- jsFileText.js
+++ jsFileText.js
@@ -19038,3 +19038,10 @@ const data = {
 const template = templater(tmpl);
 document.write(template(data));
 
+const tmpl = "<h1>Hi, I'm {{name}}.</h1>";
+const data = {
+  name: "Mubashir Iqbal"
+};
+const template = templater(tmpl);
+document.write(template(data));
+
