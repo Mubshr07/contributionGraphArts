d18c8e167283d018ef4dd202f9eddf4703785c86
Mubashir Iqbal <mubshr07@gmail.com>
2024-05-13 00:00:00 +0500

    Saving progress on 2024-05-13 00:00:00 time 233

--- qtCppFileText.cpp
+++ qtCppFileText.cpp
@@ -172668,3 +172668,34 @@ int main() {
     return 0;
 }
 
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Array {
+private:
+    T* ptr;
+    int size;
+
+public:
+    Array(T arr[], int s) {
+        ptr = new T[s];
+        size = s;
+        for (int i = 0; i < size; i++)
+            ptr[i] = arr[i];
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++)
+            cout << " " << *(ptr + i);
+        cout << endl;
+    }
+};
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    Array<int> a(arr, 5);
+    a.print(); // Output: 1 2 3 4 5
+    return 0;
+}
+
